fd35ee19287206242bc22b10c90d4c9a3d4e8b1b
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-10-14 09:39:37 -0400

    add file

--- cs1-1.py
+++ cs1-1.py
@@ -5,9 +5,7 @@ x2 = int(input("what is the value of x2: "))
 y1 = int(input("what is the value of y1: "))
 y2 = int(input("what is the value of y2: "))
 
-total = (x2 - x1) ** 2 + (y2 - y1) ** 2
-
-total = total ** .05
+total = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** .5
 
 total = str(total)
 
